f857e456f50c811a8f800bd37e866083b7e404a2
vcc-cesar <vcc.cesar@gmail.com>
2024-11-18 22:45:58 -0300

    Alterações Layout

--- views/header.php
+++ views/header.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Controle de Etiquetas</title>
+    <title>SubLev</title>
 
     <!-- Custom fonts for this template-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css?v=<?=date('YmdHis')?>" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
