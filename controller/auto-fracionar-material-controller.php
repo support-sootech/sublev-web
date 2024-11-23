6a9b9a1f88c757dfa8561a8125c9b7f4d14f6542
Anderson <andersonrobertobru@gmail.com>
2024-11-23 10:31:24 -0300

    FIX

--- controller/auto-fracionar-material-controller.php
+++ controller/auto-fracionar-material-controller.php
@@ -123,7 +123,8 @@ $app->get('/fracionar-imprimir-material/:id_materiais', function($id_materiais='
                         'margin_bottom' => 5,
                         'margin_header' => 5,
                         'margin_footer' => 5,
-                        'tempDir'=> sys_get_temp_dir().DIRECTORY_SEPARATOR.'mpdf'
+                        //'tempDir'=> sys_get_temp_dir().DIRECTORY_SEPARATOR.'mpdf'
+                        'tempDir'=> '/temp'
                     ]
                 );
 
