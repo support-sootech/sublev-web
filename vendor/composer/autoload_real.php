6df0b5be115b328d795658fb51f8c34d3a444d93
root <root@srv644445.hstgr.cloud>
2025-01-01 14:17:40 +0000

    FIX

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,25 +33,18 @@ class ComposerAutoloaderInit15f747f50e8fe54bdee611fdde340736
 
         $loader->register(true);
 
-        $includeFiles = \Composer\Autoload\ComposerStaticInit15f747f50e8fe54bdee611fdde340736::$files;
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire15f747f50e8fe54bdee611fdde340736($fileIdentifier, $file);
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit15f747f50e8fe54bdee611fdde340736::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
-    }
-}
-
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
-function composerRequire15f747f50e8fe54bdee611fdde340736($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-        require $file;
     }
 }
