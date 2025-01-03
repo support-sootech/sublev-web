5ec361f7a179370a5e88ca88bd602e06f5e2ccd4
Anderson <andersonrobertobru@gmail.com>
2025-01-03 16:36:07 -0300

    FIX - includes js e css

--- views/footer.php
+++ views/footer.php
@@ -30,27 +30,27 @@
     </div>    
     
     <!-- Bootstrap core JavaScript-->
-    <script src="<?=site_url()?>/layout/vendor/jquery/jquery.min.js"></script>
-    <script src="<?=site_url()?>/layout/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="./layout/vendor/jquery/jquery.min.js"></script>
+    <script src="./layout/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="<?=site_url()?>/layout/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="./layout/vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="<?=site_url()?>/layout/js/sb-admin-2.min.js"></script>
+    <script src="./layout/js/sb-admin-2.min.js"></script>
 
     <!-- Page level plugins -->
     <script src="<?=site_url()?>/layout/vendor/chart.js/Chart.min.js"></script>
 
     <!-- EXTRAS -->
-    <script src="<?=site_url()?>/layout/js/jquery.form.js?v=<?=date('YmdHis')?>"></script>
-    <script src="<?=site_url()?>/layout/js/jquery.maskMoney.js?v=<?=date('YmdHis')?>"></script>
-    <script src="<?=site_url()?>/layout/js/jquery.preloaders.js?v=<?=date('YmdHis')?>"></script>
-    <script src="<?=site_url()?>/layout/js/pnotify.min.js?v=<?=date('YmdHis')?>"></script>
-    <script src="<?=site_url()?>/layout/js/jgrowl.min.js?v=<?=date('YmdHis')?>"></script>
+    <script src="./layout/js/jquery.form.js?v=<?=date('YmdHis')?>"></script>
+    <script src="./layout/js/jquery.maskMoney.js?v=<?=date('YmdHis')?>"></script>
+    <script src="./layout/js/jquery.preloaders.js?v=<?=date('YmdHis')?>"></script>
+    <script src="./layout/js/pnotify.min.js?v=<?=date('YmdHis')?>"></script>
+    <script src="./layout/js/jgrowl.min.js?v=<?=date('YmdHis')?>"></script>
     <script src="https://cdn.datatables.net/2.0.3/js/dataTables.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-jgrowl/1.4.8/jquery.jgrowl.min.js"></script>
-    <!-- <script src="<?=site_url()?>/layout/js/sweetalert2.min.js?v=<?=date('YmdHis')?>"></script> -->
+    <!-- <script src="./layout/js/sweetalert2.min.js?v=<?=date('YmdHis')?>"></script> -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.js"></script>
     
     <!-- <script src="//cdnjs.cloudflare.com/ajax/libs/jquery.maskedinput/1.4.1/jquery.maskedinput.min.js"></script> -->
